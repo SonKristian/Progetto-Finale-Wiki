e93e308c710b89701ac260a22e4263a75cbfbebe
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-06-20 15:10:29 +0200

    Carousel + inizio header  + icon

--- frontend/pages/home/Home.jsx
+++ frontend/pages/home/Home.jsx
@@ -1,6 +1,13 @@
+import Carousel from "./Main/Carousel"
+import Header from "./Header/Header.jsx"
+
 const Home = () => {
   return (
+    <>
+    <Header />
     <div>Home</div>
+    <Carousel />
+    </>
   )
 }
 

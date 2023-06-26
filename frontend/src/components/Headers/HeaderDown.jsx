b856f1b536c267bb4b59e2405844cc1de72f68af
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-06-27 00:45:05 +0200

    Provato a fixare header + to do react navigation

--- frontend/src/components/Headers/HeaderDown.jsx
+++ frontend/src/components/Headers/HeaderDown.jsx
@@ -23,34 +23,30 @@ const HeaderDown = ({active}) => {
   };
 
   return (
-    <nav className={active ? "open" : "close"}>
-      {/* {console.log(active)} */}
-        <div>
-         <ul id="submenu">
-         <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/cards">CardsPage</Link>
-            </li>
-                <li>
-            <div className="dropdown" onClick={toggleDropdown}>
-              <button>Genere</button>
-              {isOpen && (
-                <ul className="dropdown-menu">
-                  {categories.map((category) => (
-                    <li key={category}>
-                      <Link to={`/category/${encodeURIComponent(category)}`}>{category}</Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+<nav className={active ? "open" : "close"}>
+  <div>
+    <ul id="submenu">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/cards">CardsPage</Link>
+      </li>
+      <li className="headerdown" onClick={toggleDropdown}>
+        <button id="list">Genere</button>
+        {isOpen && (
+          <ul className="bg-zinc-900">
+            {categories.map((category) => (
+              <li key={category}>
+                <Link to={`/category/${encodeURIComponent(category)}`}>{category}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+    </ul>
+  </div>
+</nav>
+  )
 }
-
 export default HeaderDown;

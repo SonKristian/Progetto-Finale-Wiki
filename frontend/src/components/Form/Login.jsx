68942c902511f9878000603aec943d48c66fe2bf
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-06-29 14:57:00 +0200

    Register - Form struttra html

--- frontend/src/components/Form/Login.jsx
+++ frontend/src/components/Form/Login.jsx
@@ -1,9 +1,103 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
 
-export default Login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        setLoginStatus("Login successful");
+        // Clear form inputs
+        setEmail("");
+        setPassword("");
+      } else {
+        setLoginStatus("Login failed");
+      }
+    } catch (error) {
+      setLoginStatus("Error occurred during login");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div className="flex flex-col">
+          <h1>Login</h1>
+          <p>Please enter your credentials to log in.</p>
+          <hr />
+
+            <label htmlFor="username">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <label htmlFor="password">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <hr />
+          <p>{loginStatus}</p>
+
+          <button type="submit" className="loginbtn">
+            Login
+          </button>
+        </div>
+
+        <div className="container signin">
+          <p> Do not have an account? <Link to="/register">Register</Link></p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/components/Form/Register.jsx
+++ frontend/src/components/Form/Register.jsx
@@ -1,34 +1,130 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [registrationStatus, setRegistrationStatus] = useState("");
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    if (password !== repeatPassword) {
+      setRegistrationStatus("Passwords do not match");
+      return;
+    }
+
+    const userData = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        setRegistrationStatus("Registration successful");
+        // Clear form inputs
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setRepeatPassword("");
+      } else {
+        setRegistrationStatus("Registration failed");
+      }
+    } catch (error) {
+      setRegistrationStatus("Error occurred during registration");
+      console.error(error);
+    }
+  };
+
   return (
+    <div>
+      <form onSubmit={handleRegistration}>
+        <div className="flex flex-col">
+          <h1>Register</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr />
 
-<form action="/action_page.php">
-  <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+          <label htmlFor="username">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+          <label htmlFor="password">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-    <hr>
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+          <label htmlFor="repeat-password">
+            <b>Repeat Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            name="repeat-password"
+            id="repeat-password"
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            required
+          />
+          <hr />
+          <p>{registrationStatus}</p>
+          <p>
+            By creating an account you agree to our <a href="#">Terms & Privacy</a>.
+          </p>
 
-    <button type="submit" class="registerbtn">Register</button>
-  </div>
-  
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
-  </div>
-</form>
+          <button type="submit" className="registerbtn">
+            Register
+          </button>
+        </div>
 
-  )
-}
+        <div className="container signin">
+          <p>
+            Already have an account? <Link to="/login">Sign in</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;

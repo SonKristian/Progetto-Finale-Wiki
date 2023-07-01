9ad945803edaa63da2cfad92ba78dddeae9a6fed
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-07-01 12:05:15 +0200

    Link ai carousel

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Link } from "react-router-dom";
 import "./carousel.css";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,10 +11,31 @@ import { Navigation } from "swiper";
 
 const Carousel = () => (
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide><img id="marvel" src=".\src\assets\Marvel.png" /></SwiperSlide>
-      <SwiperSlide><img id="dc" src=".\src\assets\dc.png" /></SwiperSlide>
-      <SwiperSlide><img id="lucas" src=".\src\assets\Lucasfilm.svg" /></SwiperSlide>
-      <SwiperSlide><img id="shueisha" src=".\src\assets\Shueisha.svg" /></SwiperSlide>
+    
+      <SwiperSlide>
+        <Link to="/category/Marvel Comics">
+          <img id="marvel" src=".\src\assets\Marvel.png" />
+        </Link>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Link to="/category/DC Comics">
+          <img id="dc" src=".\src\assets\dc.png" />
+        </Link>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Link to="/category/George Lucas">
+          <img id="lucas" src=".\src\assets\Lucasfilm.svg" />
+        </Link>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Link to="/category/Shueisha">
+          <img id="shueisha" src=".\src\assets\Shueisha.svg" />
+        </Link>    
+      </SwiperSlide>
+
     </Swiper>
 );
 

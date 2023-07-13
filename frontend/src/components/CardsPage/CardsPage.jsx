9ba5be90468c93c90a99b9bb779d14006eda6df8
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-07-13 19:48:37 +0200

    modifiche favorite finite

--- frontend/src/components/CardsPage/CardsPage.jsx
+++ frontend/src/components/CardsPage/CardsPage.jsx
@@ -1,18 +1,17 @@
 import Cards from "./Cards.jsx";
 import "./css/cardspage.css";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import DeleteIcon from '@mui/icons-material/Delete';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 
 const CardsPage = ({ isLoggedIn }) => {
   const [hero, setHero] = useState({});
   const { id } = useParams();
   const [activeSection, setActiveSection] = useState(null);
-  const [favorite, setsFavorite] = useState(false)
-  const storedToken = sessionStorage.getItem("jwtToken")
-  const storedUser = sessionStorage.getItem("user")
+  const [favorite, setsFavorite] = useState(false);
+  const storedToken = sessionStorage.getItem("jwtToken");
+  const storedUser = sessionStorage.getItem("user");
   // Funzione per salvare l'ID dell'eroe nei preferiti dell'utente
 
   const addFavorites = async () => {
@@ -43,7 +42,7 @@ const CardsPage = ({ isLoggedIn }) => {
         {
           headers: {
             Authorization: `Bearer ${storedToken}`,
-            id: id
+            id: id,
           },
         }
       );
@@ -54,8 +53,7 @@ const CardsPage = ({ isLoggedIn }) => {
       setHero({});
     }
   };
-  
-  
+
   useEffect(() => {
     async function getHero() {
       try {
@@ -72,8 +70,30 @@ const CardsPage = ({ isLoggedIn }) => {
   }, [id]);
 
   const toggleSection = (section) => {
-    setActiveSection((prevSection) => (prevSection === section ? null : section));
+    setActiveSection((prevSection) =>
+      prevSection === section ? null : section
+    );
   };
+
+  useEffect(() => {
+    async function handleAlreadyFavorite() {
+      if (isLoggedIn) {
+        const res = await axios.get(
+          `http://localhost:3000/favorite/${storedUser}`,
+          {
+            headers: {
+              Authorization: `Bearer ${storedToken}`,
+              id: id,
+            },
+          }
+        );
+        const favorites = res.data.includes(id);
+        console.log(favorites);
+        favorites ? setsFavorite(true) : setsFavorite(false);
+      }
+    }
+    handleAlreadyFavorite();
+  }, [id]);
 
   return (
     <div className="flex justify-around mt-5 mb-[5rem] bg-slate-400">
@@ -81,20 +101,13 @@ const CardsPage = ({ isLoggedIn }) => {
       <div>
         <div className="flex justify-center items-center ml-[4rem] text-[30px]">
           {!isLoggedIn ? (
+            <h2 className="font-extrabold text-3xl">{hero.name}</h2>
+          ) : (
+            <div className="flex items-center justify-around gap-[6.5rem] mt-2">
               <h2 className="font-extrabold text-3xl">{hero.name}</h2>
-            ) : (
-              <div className="flex items-center justify-around gap-[6.5rem] mt-2">
-              <h2 className="font-extrabold text-3xl">{hero.name}</h2>
-              {!favorite ? (
-              <p className="w-[30px] cursor-pointer" onClick={addFavorites}><FavoriteIcon /></p>
-              ):(
-                <div className="flex gap-[1rem]">
-                <p className="w-[30px] cursor-pointer" onClick={addFavorites}><FavoriteIcon /></p>
-                <p className="w-[30px] cursor-pointer" onClick={removeFavorites}><DeleteIcon /></p>
-                </div>
-              )}
-              </div>
-            )}
+                <p className={favorite ? "fav-on" : "fav-off"} onClick={!favorite ? addFavorites : removeFavorites}><FavoriteIcon /></p>
+            </div>
+          )}
         </div>
         {hero.image && <Cards size="big" url={hero.image.url} />}
       </div>
@@ -103,158 +116,169 @@ const CardsPage = ({ isLoggedIn }) => {
         <div className="flex justify-around gap-[4.2rem] text-[20px]">
           <div>
             <button
-              className={`section-btn ${activeSection === "powerstat" ? "active" : ""}`}
+              className={`section-btn ${
+                activeSection === "powerstat" ? "active" : ""
+              }`}
               onClick={() => toggleSection("powerstat")}
             >
               powerstats
             </button>
             {activeSection === "powerstat" && (
               <div className="section-content">
-              <div>
-              <h2>Intelligence</h2>
-              <meter
-                id="intelligence"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.intelligence}
-                max="100"
-              ></meter>
-            </div>
-            <div>
-              <h2>Strength</h2>
-              <meter
-                id="strength"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.strength}
-                max="100"
-              ></meter>
-            </div>
-            <div>
-              <h2>Speed</h2>
-              <meter
-                id="speed"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.speed}
-                max="100"
-              ></meter>
-            </div>
-            <div>
-              <h2>Durability</h2>
-              <meter
-                id="durability"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.durability}
-                max="100"
-              ></meter>
-            </div>
-            <div>
-              <h2>Power</h2>
-              <meter
-                id="power"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.power}
-                max="100"
-              ></meter>
-            </div>
-            <div>
-              <h2>Combat</h2>
-              <meter
-                id="combat"
-                className="progressbar"
-                value={hero.powerstats && hero.powerstats.combat}
-                max="100"
-              ></meter>
-            </div>
+                <div>
+                  <h2>Intelligence</h2>
+                  <meter
+                    id="intelligence"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.intelligence}
+                    max="100"
+                  ></meter>
+                </div>
+                <div>
+                  <h2>Strength</h2>
+                  <meter
+                    id="strength"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.strength}
+                    max="100"
+                  ></meter>
+                </div>
+                <div>
+                  <h2>Speed</h2>
+                  <meter
+                    id="speed"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.speed}
+                    max="100"
+                  ></meter>
+                </div>
+                <div>
+                  <h2>Durability</h2>
+                  <meter
+                    id="durability"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.durability}
+                    max="100"
+                  ></meter>
+                </div>
+                <div>
+                  <h2>Power</h2>
+                  <meter
+                    id="power"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.power}
+                    max="100"
+                  ></meter>
+                </div>
+                <div>
+                  <h2>Combat</h2>
+                  <meter
+                    id="combat"
+                    className="progressbar"
+                    value={hero.powerstats && hero.powerstats.combat}
+                    max="100"
+                  ></meter>
+                </div>
               </div>
             )}
           </div>
 
           <div>
             <button
-              className={`section-btn ${activeSection === "biography" ? "active" : ""}`}
+              className={`section-btn ${
+                activeSection === "biography" ? "active" : ""
+              }`}
               onClick={() => toggleSection("biography")}
             >
               biography
             </button>
             {activeSection === "biography" && (
               <div className="section-content">
-              <ul className="flex flex-col">
-              <li>
-                Full Name: {hero.biography && hero.biography["full-name"]}
-              </li>
-              <li>
-                Alter Egos: {hero.biography && hero.biography["alter-egos"]}
-              </li>
-              <li>
-                Aliases: {hero.biography && hero.biography.aliases.join(", ")}
-              </li>
-              <li>
-                Place of Birth:{" "}
-                {hero.biography && hero.biography["place-of-birth"]}
-              </li>
-              <li>
-                First Appearance:{" "}
-                {hero.biography && hero.biography["first-appearance"]}
-              </li>
-              <li>Publisher: {hero.biography && hero.biography.publisher}</li>
-              <li>Alignment: {hero.biography && hero.biography.alignment}</li>
-            </ul>
+                <ul className="flex flex-col">
+                  <li>
+                    Full Name: {hero.biography && hero.biography["full-name"]}
+                  </li>
+                  <li>
+                    Alter Egos: {hero.biography && hero.biography["alter-egos"]}
+                  </li>
+                  <li>
+                    Aliases:{" "}
+                    {hero.biography && hero.biography.aliases.join(", ")}
+                  </li>
+                  <li>
+                    Place of Birth:{" "}
+                    {hero.biography && hero.biography["place-of-birth"]}
+                  </li>
+                  <li>
+                    First Appearance:{" "}
+                    {hero.biography && hero.biography["first-appearance"]}
+                  </li>
+                  <li>
+                    Publisher: {hero.biography && hero.biography.publisher}
+                  </li>
+                  <li>
+                    Alignment: {hero.biography && hero.biography.alignment}
+                  </li>
+                </ul>
               </div>
             )}
           </div>
 
           <div>
             <button
-              className={`section-btn ${activeSection === "appearance" ? "active" : ""}`}
+              className={`section-btn ${
+                activeSection === "appearance" ? "active" : ""
+              }`}
               onClick={() => toggleSection("appearance")}
             >
               appearance
             </button>
             {activeSection === "appearance" && (
               <div className="section-content">
-              <ul className="flex flex-col">
-              <li>
-                Gender: {hero.appearance && hero.appearance.gender}
-              </li>
-              <li>
-               Race: {hero.appearance && hero.appearance.race}
-              </li>
-              <li>
-                Height: {hero.appearance && hero.appearance.height.join(", ")}
-              </li>
-              <li>
-                Weight:{" "}
-                {hero.appearance && hero.appearance.weight.join(", ")}
-              </li>
-              <li>
-                Work:
-                {hero.work && hero.work.occupation}
-              </li>
-              <li>
-                Base:
-                {hero.work && hero.work.base}
-              </li>
-            </ul>
+                <ul className="flex flex-col">
+                  <li>Gender: {hero.appearance && hero.appearance.gender}</li>
+                  <li>Race: {hero.appearance && hero.appearance.race}</li>
+                  <li>
+                    Height:{" "}
+                    {hero.appearance && hero.appearance.height.join(", ")}
+                  </li>
+                  <li>
+                    Weight:{" "}
+                    {hero.appearance && hero.appearance.weight.join(", ")}
+                  </li>
+                  <li>
+                    Work:
+                    {hero.work && hero.work.occupation}
+                  </li>
+                  <li>
+                    Base:
+                    {hero.work && hero.work.base}
+                  </li>
+                </ul>
               </div>
             )}
           </div>
 
           <div>
             <button
-              className={`section-btn ${activeSection === "connection" ? "active" : ""}`}
+              className={`section-btn ${
+                activeSection === "connection" ? "active" : ""
+              }`}
               onClick={() => toggleSection("connection")}
             >
               connections
             </button>
             {activeSection === "connection" && (
               <div className="section-content">
-              <ul className="flex flex-col">
-              <li>
-                Group Affiliation: {hero.connections && hero.connections["group-affiliation"]}
-              </li>
-              <li>
-              Relatives: {hero.connections && hero.connections.relatives}
-              </li>
-            </ul>
+                <ul className="flex flex-col">
+                  <li>
+                    Group Affiliation:{" "}
+                    {hero.connections && hero.connections["group-affiliation"]}
+                  </li>
+                  <li>
+                    Relatives: {hero.connections && hero.connections.relatives}
+                  </li>
+                </ul>
               </div>
             )}
           </div>
